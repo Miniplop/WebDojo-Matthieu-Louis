334213b97ae4f3cbd398f58fd0c9bee991499e5e
Kevin <kevinj@theodo.fr>
2017-05-30 21:24:18 +0200

    Token

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,7 +6,7 @@ var config = require('../config/default.json');
 router.get('/', function(req, res, next) {
   console.log(config);
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === config) {
+      req.query['hub.verify_token'] === config.validationToken) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
