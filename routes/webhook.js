b3116b4bb7273cf2d3b4c9ab057a683a8de7e25f
Kevin <kevinj@theodo.fr>
2017-05-30 22:02:21 +0200

    suite

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config/default.json');
+var chatService = require('../server/chatService')
+
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
   console.log(config);
-  if (authenticate(req)) {
+  if (chatService.authenticate(req)) {
     res.status(200).send("hello");
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
